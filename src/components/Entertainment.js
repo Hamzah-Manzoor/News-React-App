54ca98958f5f35b1c764d6251364ec8c360aeb22
Hamza4241 <hamza88851@gmail.com>
2023-08-07 15:35:30 +0500

    Implemented Home and Categories pages

---
+++ src/components/Entertainment.js
@@ -0,0 +1,57 @@
+import { React, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchNews } from '../redux/counter';
+
+export default function Entertainment() {
+
+    const hasRun = useRef(false);
+    const resultCount = useSelector((state) => state.counter.totalResults);
+    const newsArray = useSelector((state) => state.counter.newsArray);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!hasRun.current) {
+          dispatch(fetchNews("entertainment"));
+          hasRun.current = true;
+        }
+      }, []);
+
+
+  return (
+    <div className='entertainment-page'>
+
+      <div className='entertainment-news flex flex-row flex-wrap justify-center'>
+        {newsArray.map((news, id) => (
+
+              <div className='bg-slate-700 w-96 rounded-2xl py-2 mx-2 my-4 px-3 flex flex-col sm:justify-between 
+              h-fit sm:items-center hover:bg-slate-800 active:bg-slate-900 focus:bg-slate-900 focus:border-blue-500' key={id}>
+
+                <div className='image-btns justify-between items-center py-3'>
+                  <div className='image h-2/6'>
+                    {news.urlToImage ? ( 
+                      <img className='h-5/6' src={news.urlToImage} alt="Could not fetch image" />
+                    ): <img className='h-5/6' src='https://th.bing.com/th/id/R.50428446fe1a2469bed28c5db4166ca6?rik=wTrZv%2bwhoFkmXw&riu=http%3a%2f%2fdehayf5mhw1h7.cloudfront.net%2fwp-content%2fuploads%2fsites%2f114%2f2017%2f10%2f02065917%2fbreaking-news-shutterstock.jpg&ehk=0gOFnAI5ADyEz1nv2r7WHW7VmbfFq4t3MXwfIC9vNRc%3d&risl=&pid=ImgRaw&r=0' alt="Could not fetch image" /> }
+                    
+                  </div>
+                </div>
+
+                <div className='title font-bold text-2xl mb-7'>
+                    {news.title}
+                </div>
+
+                <div className='description text-xl'>
+                    {news.description}
+                </div>
+
+                <div className='btn mt-4 mb-3'>
+                    <button className='button rounded-2xl inline-block align-middle font-bold text-base bg-blue-700 px-3 py-1 hover:bg-blue-800 focus:bg-slate-900 focus:border-blue-500 mr-2' 
+                        onClick={() => window.open(news.url, '_blank')}>
+                        Read more <span className='font-black'>&#x2192;</span>
+                    </button>
+                </div>
+              </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/components/News.js
+++ src/components/News.js
@@ -11,38 +11,92 @@ export default function News() {
 
   useEffect(() => {
     if (!hasRun.current) {
-      dispatch(fetchNews());
+      dispatch(fetchNews("general"));
       hasRun.current = true;
     }
   }, []);
 
   return (
-    <div>
-      <div>
 
+    <div className='home-page mx-4'>
+
+      <div className='general-news flex flex-row flex-wrap mr-2'>
         {newsArray.map((news, id) => (
-          <div className='city-history bg-slate-700 rounded-2xl py-2 my-4 px-5 flex flex-col sm:flex-row sm:justify-between 
-          h-fit sm:items-center hover:bg-slate-800 active:bg-slate-900 focus:bg-slate-900 focus:border-blue-500' key={id}>
-            <div className='name-temp sm:basis-2/5 flex flex-row justify-between items-center'>
-              <div className='name font-bold text-2xl'>
-                {news.title}
-              </div>
+          <>
+            {/* {news.author !== "BBC Sport" ? ( */}
+              <div className='bg-slate-700 rounded-2xl py-2 my-4 px-5 flex flex-col sm:flex-row sm:justify-between 
+              h-fit sm:items-center hover:bg-slate-800 active:bg-slate-900 focus:bg-slate-900 focus:border-blue-500' key={id}>
 
-              <div className='temp font-bold text-2xl'>
-                {news.description}
-              </div>
-            </div>
+                <div className='name-temp sm:basis-2/5 flex flex-col justify-between items-center'>
+                  <div className='name font-bold text-2xl mb-7'>
+                    {news.title}
+                  </div>
 
-            <div className='image-btns sm:basis-3/5 flex flex-row justify-between items-center'>
-              <div className='image'>
-                <img className='h-5/6' src={news.urlToImage} alt="Image" />
-              </div>
-            </div>
+                  <div className='temp text-xl'>
+                    {news.description}
+                  </div>
 
-          </div>  
+                  <div className='btn mt-4 mb-3'>
+                    <button className='button rounded-2xl inline-block align-middle font-bold text-base bg-blue-700 px-3 py-1 hover:bg-blue-800 focus:bg-slate-900 focus:border-blue-500 mr-2' 
+                      onClick={() => window.open(news.url, '_blank')}>
+                      Read more <span className='font-black'>&#x2192;</span>
+                    </button>
+
+                  </div>
+                </div>
+
+                <div className='image-btns sm:basis-3/5 justify-between items-center pl-4 pr-1 py-3'>
+                  <div className='image h-2/6'>
+                    {news.urlToImage ? ( 
+                      <img className='h-5/6' src={news.urlToImage} alt="Could not fetch image" />
+                    ): <img className='h-5/6' src='https://th.bing.com/th/id/R.50428446fe1a2469bed28c5db4166ca6?rik=wTrZv%2bwhoFkmXw&riu=http%3a%2f%2fdehayf5mhw1h7.cloudfront.net%2fwp-content%2fuploads%2fsites%2f114%2f2017%2f10%2f02065917%2fbreaking-news-shutterstock.jpg&ehk=0gOFnAI5ADyEz1nv2r7WHW7VmbfFq4t3MXwfIC9vNRc%3d&risl=&pid=ImgRaw&r=0' alt="Could not fetch image" /> }
+                    
+                  </div>
+                </div>
+              </div>
+            {/* ) : null } */}
+          </>
         ))}
-
       </div>
+
+      {/* <div className='sports-news basis-1/3 mt-3'>
+        <div className='heading text-center text-gray-400 text-xl font-bold'>Sports News</div>
+        {newsArray.map((news, id) => (
+          <>
+            {news.author == "BBC Sport" ? (
+              <div className='bg-slate-700 rounded-2xl py-2 my-4 px-5 flex flex-col sm:justify-between 
+              h-fit sm:items-center hover:bg-slate-800 active:bg-slate-900 focus:bg-slate-900 focus:border-blue-500' key={id}>
+
+                <div className='name-temp sm:basis-2/5 flex flex-col justify-between items-center'>
+                  <div className='name font-bold text-2xl mb-7'>
+                    {news.title}
+                  </div>
+
+                  <div className='temp text-xl'>
+                    {news.description}
+                  </div>
+
+                  <div className='btn mt-4 mb-5'>
+                    <button className='button rounded-2xl inline-block align-middle font-bold text-base bg-blue-700 px-3 py-1 hover:bg-blue-800 focus:bg-slate-900 focus:border-blue-500 mr-2' 
+                      onClick={() => window.open(news.url, '_blank')}>
+                      Read more <span className='font-black'>&#x2192;</span>
+                    </button>
+
+                  </div>
+                </div>
+
+                <div className='image-btns sm:basis-3/5 justify-between items-center px-4'>
+                  <div className='image h-2/6'>
+                    <img className='h-5/6' src={news.urlToImage} alt="Image" />
+                  </div>
+                </div>
+              </div>
+            ) : null }
+          </>
+        ))}
+      </div> */}
+
+
     </div>
   )
 }
